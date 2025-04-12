b8f860c09bd3641d4d4503f2d58754c7bb365757
gmsahu <163687909+gmsahu@users.noreply.github.com>
2025-04-11 19:33:52 -0600

    Update financial.py

--- financial.py
+++ financial.py
@@ -15,7 +15,7 @@ web_search_agent=Agent(
     role="Search the web for the information",
     model=Groq(id="llama-3.3-70b-versatile"),
     tools=[DuckDuckGo()],
-    instructions=["Alway include sources"],
+    instructions=["Always include sources"],
     show_tools_calls=True,
     markdown=True,
 
